746bbb0c8e8c4a4283b3d583654466020e5767f6
Sangjae Kim <kimsangjae94114@gmail.com>
2026-01-19 02:51:32 +0900

    리스트 상세조회 구현

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/authStore'
 
 import CrmLayout from '@/layouts/CrmLayout.vue'
-import ActivityLayout from '@/views/activity/ActivityLayout.vue'
+import ActivityLayout from '@/views/activity/view/ActivityLayout.vue'
 import ReportLayout from '@/views/report/ReportLayout.vue'
 import CustomerLayout from '@/views/customer/CustomerLayout.vue'
 import MessageLayout from '@/views/message/MessageLayout.vue'
@@ -36,10 +36,10 @@ const routes = [
                 component: ActivityLayout,
                 redirect: '/activities/all',
                 children: [
-                    { path: 'all', component: () => import('@/views/activity/ActivityAllView.vue') },
-                    { path: 'check', component: () => import('@/views/activity/ActivityCheckView.vue') },
-                    { path: 'facility', component: () => import('@/views/activity/ActivityFacilityView.vue') },
-                    { path: 'timeline', component: () => import('@/views/activity/ActivityTimelineView.vue') },
+                    { path: 'all', component: () => import('@/views/activity/view/ActivityAllView.vue') },
+                    { path: 'check', component: () => import('@/views/activity/view/ActivityCheckView.vue') },
+                    { path: 'facility', component: () => import('@/views/activity/view/ActivityFacilityView.vue') },
+                    { path: 'timeline', component: () => import('@/views/activity/view/ActivityTimelineView.vue') },
                 ],
             },
 
